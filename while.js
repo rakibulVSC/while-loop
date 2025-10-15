3c086c92e6237179cd0617eb3c32a7b123630c7d
rakibulVSC <mrakibulhasan755@gmail.com>
2025-10-15 09:11:37 +0600

    sum of array in easy

---
+++ while.js
@@ -0,0 +1,34 @@
+// const arr1=[1,2,3];
+// const arr2=[4,5,6];
+// const arr=arr1.concat(arr2)
+// console.log(arr)
+
+// const numbers=[12,98,69,56,45,25]
+// console.log(numbers)
+// for (const num of numbers){
+//     console.log(num)
+// }
+
+// const fruits=['orange','apple','banana','jackfruit','watermelon'];
+// for (const fruit of fruits){
+//     console.log(fruit)
+//     console.log("i want to eat")
+// }
+
+// const conutries=['bangladesh','srilanka','malaysia']
+// for (const  country of conutries){
+//     console.log(country)
+// }
+
+let num=1;
+let sum=0;
+while(num <10){
+    console.log(num);
+    sum =sum +num;
+    console.log('sum is ',sum)
+ num++;
+}
+
+
+
+
